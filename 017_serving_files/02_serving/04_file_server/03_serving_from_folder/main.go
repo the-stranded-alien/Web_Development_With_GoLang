92881316f73ad156add6ea8eb704a69df4f65afd
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-07 23:00:22 +0530

    serving files

---
+++ 017_serving_files/02_serving/04_file_server/03_serving_from_folder/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", dog)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8888", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<img src="/assets/toby.jpg">
+	`)
+}
